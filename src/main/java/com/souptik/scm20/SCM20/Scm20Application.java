cf7896f20b47c02ebfd293e66f5c6dd75bcc47a6
souptik23 <souptikkaran23@gmail.com>
2024-09-25 16:51:12 +0530

    sub branch comment

--- src/main/java/com/souptik/scm20/SCM20/Scm20Application.java
+++ src/main/java/com/souptik/scm20/SCM20/Scm20Application.java
@@ -8,6 +8,10 @@ public class Scm20Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Scm20Application.class, args);
+
+		//check command in the main branch for pushing 
+
+		//this is the comment in the sub branch
 	}
 
 }
