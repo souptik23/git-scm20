389f608191837b135fb7c5c4b515f32de502ee69
souptik23 <souptikkaran23@gmail.com>
2024-09-25 16:44:01 +0530

    intial

---
+++ src/main/java/com/souptik/scm20/SCM20/Scm20Application.java
@@ -0,0 +1,13 @@
+package com.souptik.scm20.SCM20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scm20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scm20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/souptik/scm20/SCM20/Scm20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souptik.scm20.SCM20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scm20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
